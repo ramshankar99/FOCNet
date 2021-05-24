dbd4042813fb00a53660de875e53afece075ef7e
Ramachandren Shankar <43037428+ramshankar99@users.noreply.github.com>
2021-05-24 13:21:04 -0500

    focnet accepts train and test images

--- generatepatches.m
+++ generatepatches.m
@@ -2,98 +2,61 @@ function [imdb] = generatepatches
 
 addpath('utilities');
 batchSize     = 8;        % batch size
-global CurTask
-if strcmpi(CurTask, 'Denoising')
-%     folder    = 'Train_Set';  %
-    folder    = 'BSDS300';
-    stride    = 130;
-    scales  = [1]; % scale the image to augment the training data
-else
-    folder = 'Train_Set';
-    stride = 100;
-    scales  = [1]; % scale the image to augment the training data
 
-end
+folder = 'Train_Set';  
+label_folder = 'Train_Set_Label';
+
 nchannel      = 1;           % number of channels
 patchsize     = 160;
 
-step1         = randi(stride)-1;
-step2         = randi(stride)-1;
-count         = 0;
 ext           =  {'*.jpg','*.png','*.bmp','*.jpeg'};
 filepaths     =  [];
+label_filepaths = [];
 
 for i = 1 : length(ext)
     filepaths = cat(1,filepaths, dir(fullfile(folder, ext{i})));
 end
 
+
+for i = 1 : length(ext)
+    label_filepaths = cat(1,label_filepaths, dir(fullfile(label_folder, ext{i})));
+end
+
 % count the number of extracted patches
 for i = 1 : length(filepaths)
     
-    image = imread(fullfile(folder,filepaths(i).name)); % uint8
-    if size(image,3)==3
-        if strcmpi(CurTask, 'Denoising')
-            image = rgb2gray(image);  %
-        else
-            image = rgb2ycbcr(image);
-            image = image(:,:,1);
-        end
-    end
+    %image = imread(fullfile(folder,filepaths(i).name)); % uint8
+    %label = imread(fullfile(label_folder,label_filepaths(i).name)); % uint8
     %[~, name, exte] = fileparts(filepaths(i).name);
+    
     if mod(i,100)==0
         disp([i,length(filepaths)]);
     end
-    for s = 1:length(scales)
-        image = imresize(image,scales(s),'bicubic');
-        [hei,wid,~] = size(image);
-        for x = 1+step1 : stride : (hei-patchsize+1)
-            for y = 1+step2 :stride : (wid-patchsize+1)
-                count = count+1;
-            end
-        end
-    end
 end
 
-numPatches  = ceil(count/batchSize)*batchSize;
-diffPatches = numPatches - count;
+numPatches  = 999;
+%diffPatches = numPatches - count;
 disp([int2str(numPatches),' = ',int2str(numPatches/batchSize),' X ', int2str(batchSize)]);
 
 
 count = 0;
-imdb.labels  = zeros(patchsize, patchsize, nchannel, numPatches,'single');
+imdb.images = zeros(patchsize, patchsize, nchannel, numPatches,'single');
+imdb.labels = zeros(patchsize, patchsize, nchannel, numPatches,'single');
 
 for i = 1 : length(filepaths)
     
     image = imread(fullfile(folder,filepaths(i).name)); % uint8
+    label = imread(fullfile(label_folder,label_filepaths(i).name));
     %[~, name, exte] = fileparts(filepaths(i).name);
-    if size(image,3)==3
-        if strcmpi(CurTask, 'Denoising')
-            image = rgb2gray(image);  %
-        else
-            image = rgb2ycbcr(image);
-            image = image(:,:,1);
-        end
-    end
+    
+    image = rgb2gray(image);
+    label = im2gray(label);
+    
+    count = count+1;
+    imdb.images(:, :, :, count) = image(120:120+patchsize-1,55:55+patchsize-1,:);
+    imdb.labels(:, :, :, count) = label(120:120+patchsize-1,55:55+patchsize-1,:);
     if mod(i,100)==0
         disp([i,length(filepaths)]);
-    end
-    for s = 1:length(scales)
-        image = imresize(image,scales(s),'bicubic');
-        for j = 1:1
-            image_aug   = data_augmentation(image, j);  % augment data
-            im_label    = im2single(image_aug);         % single
-            [hei,wid,~] = size(im_label);
-            
-            for x = 1+step1 : stride : (hei-patchsize+1)
-                for y = 1+step2 :stride : (wid-patchsize+1)
-                    count       = count+1;
-                    imdb.labels(:, :, :, count)   = im_label(x : x+patchsize-1, y : y+patchsize-1,:);
-                    if count<=diffPatches
-                        imdb.labels(:, :, :, end-count+1)   = im_label(x : x+patchsize-1, y : y+patchsize-1,:);
-                    end
-                end
-            end
-        end
     end
 end
 
